ee6ca6b9ab7c0ba84f9991097302b71264c98d88
Zdravko Zdravkov <zdr0@yahoo.com>
2020-05-18 19:27:11 +0200

    Parallel tasks

--- 02. WEB SERVER - ASYNCHRONOUS PROCESSING/Demos/Demo/MainWindow.xaml.cs
+++ 02. WEB SERVER - ASYNCHRONOUS PROCESSING/Demos/Demo/MainWindow.xaml.cs
@@ -3,6 +3,7 @@
     using System.IO;
     using System.Net.Http;
     using System.Threading;
+    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Media.Imaging;
@@ -17,21 +18,24 @@
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.DownloadImage(this.Image1, "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png");
-            this.DownloadImage(this.Image2, "https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg");
-            this.DownloadImage(this.Image3, "https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/cat-behaviour.jpg");
-            this.DownloadImage(this.Image4, "https://www.advantagepetcare.com.au/sites/g/files/adhwdz311/files/styles/paragraph_image/public/2019-08/cat-eating-out-of-bowl.jpg");
-            this.DownloadImage(this.Image5, "https://www.algonquinhotel.com/wp-content/uploads/sites/21/2019/07/the-algonquin-hotel-autograph-collection-cat-02-1440x900.jpg");
+            var task1 = this.DownloadImageAsync(this.Image1, "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png");
+            var task2 = this.DownloadImageAsync(this.Image2, "https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg");
+            var task3 = this.DownloadImageAsync(this.Image3, "https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/cat-behaviour.jpg");
+            var task4 = this.DownloadImageAsync(this.Image4, "https://www.advantagepetcare.com.au/sites/g/files/adhwdz311/files/styles/paragraph_image/public/2019-08/cat-eating-out-of-bowl.jpg");
+            var task5 = this.DownloadImageAsync(this.Image5, "https://www.algonquinhotel.com/wp-content/uploads/sites/21/2019/07/the-algonquin-hotel-autograph-collection-cat-02-1440x900.jpg");
+
+            //All tasks go in parallel
+            await Task.WhenAll(task1, task2, task3, task4, task5);
         }
 
-        private void DownloadImage(Image image, string url)
+        private async Task DownloadImageAsync(Image image, string url)
         {
             var client = new HttpClient();
-            Thread.Sleep(2000);
-            var response = client.GetAsync(url).GetAwaiter().GetResult();
-            var byteData = response.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+            await Task.Run(() => Thread.Sleep(2000));
+            var response = await client.GetAsync(url);
+            var byteData = await response.Content.ReadAsByteArrayAsync();
             image.Source = this.LoadImage(byteData);
         }
 
