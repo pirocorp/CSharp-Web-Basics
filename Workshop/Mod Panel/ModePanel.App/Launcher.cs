91cbb1fb7bbb1e4e5d6876935c3d423b5754ec0d
Zdravko Zdravkov <zdr0@yahoo.com>
2020-06-08 12:53:07 +0200

    Add static constructor for Launcher to auto migrate database

--- Workshop/Mod Panel/ModePanel.App/Launcher.cs
+++ Workshop/Mod Panel/ModePanel.App/Launcher.cs
@@ -1,11 +1,20 @@
 ﻿namespace ModePanel.App
 {
+    using Data;
+    using Microsoft.EntityFrameworkCore;
     using SimpleMvc.Framework;
     using SimpleMvc.Framework.Routers;
     using WebServer;
 
     public static class Launcher
     {
+        static Launcher()
+        {
+            using var db = new ModePanelDbContext();
+
+            db.Database.Migrate();
+        }
+
         public static void Main()
         {
             MvcEngine.Run(new WebServer(80, new ControllerRouter(), new ResourceRouter()));
