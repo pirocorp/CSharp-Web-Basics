b698a0ad999fcb8b6afa8504aaa0781530e5bf55
Zdravko Zdravkov <pirocorp@abv.bg>
2021-06-22 15:04:56 +0200

    CarShop exam prep

---
+++ 07. EXAM PREPARATION/Preps/CarShop/Data/DataConstants.cs
@@ -0,0 +1,25 @@
+﻿namespace CarShop.Data
+{
+    using System;
+
+    public static class DataConstants
+    {
+        public const int DefaultMaxLength = 20;
+
+        public const int DefaultMinLength = 4;
+
+        public const int IdMaxLength = 40;
+
+        public const int PlateMaxLength = 8;
+
+        public const string Client = nameof(Client);
+
+        public const string Mechanic = nameof(Mechanic);
+
+        public const string CarPlateNumberRegularExpression = @"[A-Z]{1,2}[0-9]{4}[A-Z]{2}";
+
+        public const int CarYearMinValue = 1900;
+
+        public const int CarYearMaxValueOffset = 10;
+    }
+}
---
+++ 07. EXAM PREPARATION/Preps/CarShop/Data/Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace CarShop.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using static DataConstants;
+
+    public class User
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        [Required]
+        [StringLength(IdMaxLength)]
+        public string Id { get; init; }
+
+        [Required]
+        [StringLength(DefaultMaxLength, MinimumLength = DefaultMinLength)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool IsMechanic { get; set; }
+    }
+}
