6251365af60cd47e1dd5fce6cd207a43944fd415
Zdravko Zdravkov <pirocorp@abv.bg>
2021-06-22 15:56:36 +0200

    git skeleton

---
+++ 07. EXAM PREPARATION/Preps/Git/Data/GitDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Git.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class GitDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS2019;Database=GitDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+    }
+}
